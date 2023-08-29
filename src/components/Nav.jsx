b6562ccd4f98d66c1f2e7e18ccf90335d815f5e1
manuimfeld <manu_jadir@hotmail.com>
2023-08-29 16:31:53 -0300

    Nav component

---
+++ src/components/Nav.jsx
@@ -0,0 +1,9 @@
+const Nav = () => {
+  return (
+    <nav className="p-4 bg-bgblack h-[60px] text-center">
+      <h1 className="text-pwhite">TechStack</h1>
+    </nav>
+  );
+};
+
+export default Nav;
