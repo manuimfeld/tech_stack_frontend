0fb79d3c85eb5baefd3c4c73bc26823021664a26
manuimfeld <manu_jadir@hotmail.com>
2023-08-29 17:12:51 -0300

    Add links in Home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,11 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Home = () => {
   return (
-    <main>
-      <h1>Explora el Mundo de la Programación</h1>
-      <p>
-        Descubre una amplia gama de lenguajes de programación y sumérgete en las
-        posibilidades ilimitadas que ofrecen para dar vida a tus ideas.
-      </p>
+    <main className="flex flex-col justify-center items-center bg-bgblack p-[0px_16px_16px_16px] h-[calc(100vh_-_120px)] text-center">
+      <h2 className="uppercase text-white">¿Qué estás buscando?</h2>
+      <Link
+        to="/technologys"
+        className="uppercase text-pwhite text-3xl font-semibold mt-10 flex items-center justify-center w-full h-[25%] border-2 border-primary rounded-[15px]"
+      >
+        Tecnologías
+      </Link>
+      <Link
+        to="/stacks"
+        className="uppercase text-pwhite text-3xl font-semibold mt-10 flex items-center justify-center w-full h-[25%] border-2 border-pgray rounded-[15px]"
+      >
+        Stacks
+      </Link>
     </main>
   );
 };
