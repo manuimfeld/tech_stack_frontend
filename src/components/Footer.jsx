1335eebb7e7da580847f1cb51c9cd583b838d23a
manuimfeld <manu_jadir@hotmail.com>
2023-08-29 17:12:13 -0300

    Create footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="flex items-center border-t border-pgray px-4 bg-bgblack h-[60px] text-center">
+      <p className="text-pgray">
+        @2023 Tech-stack. Todos los derechos reservados
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
