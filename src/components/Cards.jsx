ee2177edf9778fda544c4079c7bb337f3f37849a
manuimfeld <manu_jadir@hotmail.com>
2023-09-01 17:47:27 -0300

    Fix Link redirect route

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -24,7 +24,7 @@ const Cards = ({ props }) => {
           </p>
           <div className="flex items-center flex-wrap ">
             <Link
-              to={`/technologys/${props.technology}`}
+              to={`/technology/${props.technology}`}
               state={{ props: props }}
               className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0"
             >
