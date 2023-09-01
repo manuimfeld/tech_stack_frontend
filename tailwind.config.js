d59a10745ee1f1fc06d86512653f7cfe1374616b
manuimfeld <manu_jadir@hotmail.com>
2023-09-01 14:50:22 -0300

    add dependencies, and add configurations for tailwind and vite localhost

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgblack: "#0E1015",
+        pwhite: "#E2E3E7",
+        pgray: "#B7B8B9",
+        primary: "#6723FF",
+        hovprimary: "#4D12C5",
+        secondary: "#4ADE80",
+      },
+    },
   },
   plugins: [],
 };
